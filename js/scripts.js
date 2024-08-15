d89d17dc7e86b94b1f4e7fd80c1c7b4010eeb46d
tscholl44 <tscholl4444@gmail.com>
2024-08-15 17:41:52 -0400

    Updated pokedex array and added for loop w conditional

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,28 @@
+//pokedex array
 let pokemonList = [
-    {name:'Golem', height: 1.5, type:['rock', 'ground']},
-    {name:'Nidoking', height: 1.4, type: ['ground', 'poison']},
-    {name:'Wigglypuff', height: 1, type: ['fairy', 'normal']}
+    {
+        name:'Golem',
+        height: 1.5,
+        type:['rock', 'ground']
+    },
+    {
+        name:'Nidoking',
+        height: 1.4,
+        type: ['ground', 'poison']
+    },
+    {
+        name:'Wigglypuff',
+        height: 1, 
+        type: ['fairy', 'normal']
+    }
 ];
 
+//loop to iterate over each item until a specified attribute is identified
+for (let i = 0; i < pokemonList.length; i++) {
+    let pokemon = pokemonList[i];
+    if (pokemon.height >1.4){
+        document.write(pokemon.name + ' height:' + pokemon.height + ' - Wow, thats big! </br>');
+    }else {
+        document.write(pokemon.name + ' height:' + pokemon.height + '</br>');
+    }
+}
