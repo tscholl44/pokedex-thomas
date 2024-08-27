7ad71ff752e3b5bde00791cc79e4f407c2039ac7
tscholl44 <tscholl4444@gmail.com>
2024-08-27 17:15:31 -0400

    Removed for loop, replaced with forEach method to write each pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -17,12 +17,11 @@ let pokemonList = [
     }
 ];
 
-//loop to iterate over each item until a specified attribute is identified
-for (let i = 0; i < pokemonList.length; i++) {
-    let pokemon = pokemonList[i];
+function showPokemon(pokemon) {
     if (pokemon.height >1.4){
         document.write(pokemon.name + ' height:' + pokemon.height + ' - Wow, thats big! </br>');
     }else {
         document.write(pokemon.name + ' height:' + pokemon.height + '</br>');
-    }
+    } 
 }
+pokemonList.forEach(showPokemon);
