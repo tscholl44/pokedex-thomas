5cb1f64bf4692e18ca947a1241347876c63a8ba6
tscholl44 <tscholl4444@gmail.com>
2024-08-27 17:38:02 -0400

    New IIFE and functions for pokemonRepository
    Created IIFE in js file to encompass array and functions to add and display pokemon from the list
    
    foreach loop uses new variable getPokemon to access pokemonRepository.getAll

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,41 @@
 //pokedex array
-let pokemonList = [
-    {
-        name:'Golem',
-        height: 1.5,
-        type:['rock', 'ground']
-    },
-    {
-        name:'Nidoking',
-        height: 1.4,
-        type: ['ground', 'poison']
-    },
-    {
-        name:'Wigglypuff',
-        height: 1, 
-        type: ['fairy', 'normal']
-    }
-];
+//pokemon repository variable to hold the IIFE return
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name:'Golem',
+            height: 1.5,
+            type:['rock', 'ground']
+        },
+        {
+            name:'Nidoking',
+            height: 1.4,
+            type: ['ground', 'poison']
+        },
+        {
+            name:'Wigglypuff',
+            height: 1, 
+            type: ['fairy', 'normal']
+        }
+    ];
 
+//function to add a single item to the pokemonList
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+//function to return all items 
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+//forEach method to iterate through and write pokemon
 function showPokemon(pokemon) {
     if (pokemon.height >1.4){
         document.write(pokemon.name + ' height:' + pokemon.height + ' - Wow, thats big! </br>');
@@ -24,4 +43,5 @@ function showPokemon(pokemon) {
         document.write(pokemon.name + ' height:' + pokemon.height + '</br>');
     } 
 }
-pokemonList.forEach(showPokemon);
+let getPokemon = pokemonRepository.getAll()
+getPokemon.forEach(showPokemon);
