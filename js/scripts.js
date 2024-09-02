af3a697e004048a52cc7a7e46335465335919fe3
tscholl44 <tscholl4444@gmail.com>
2024-09-02 13:34:54 -0400

    button overhaul for js pokedex
    -created functions to show buttons for pokemon names and tell the console to log each click via an event listener

--- js/scripts.js
+++ js/scripts.js
@@ -3,25 +3,30 @@
 let pokemonRepository = (function () {
     let pokemonList = [
         {
-            name:'Golem',
+            name:"Golem",
             height: 1.5,
-            type:['rock', 'ground']
+            type:["rock", "ground"]
         },
         {
-            name:'Nidoking',
+            name:"Nidoking",
             height: 1.4,
-            type: ['ground', 'poison']
+            type: ["ground", "poison"]
         },
         {
-            name:'Wigglypuff',
+            name:"Wigglypuff",
             height: 1, 
-            type: ['fairy', 'normal']
+            type: ["fairy", "normal"]
         }
     ];
 
 //function to add a single item to the pokemonList
     function add(pokemon) {
-        pokemonList.push(pokemon);
+        // check if the parameter is an object and not null
+        if (typeof pokemon === "object" && pokemon !== null) {
+            pokemonList.push(pokemon);
+        } else {
+            console.log("Not an object");
+        }
     }
 
 //function to return all items 
@@ -29,19 +34,41 @@ let pokemonRepository = (function () {
         return pokemonList;
     }
 
+//function to add pokemon to the pokemon list as a list item
+    function addListItem(pokemon) {
+        let pokedex = document.querySelector(".pokemon-list");
+        let pokedexItem = document.createElement("li");
+        let button = document.createElement("button")
+        button.innerText = pokemon.name;
+        button.classList.add("button-class");
+        pokedexItem.appendChild(button);
+        pokedex.appendChild(pokedexItem);
+        //add an event listener for clicking a button
+        button.addEventListener('click', function () {
+            showDetails(pokemon);
+        });
+
+    }
+
+//function to show details using parameter: pokemon
+    function showDetails(pokemon) {
+        console.log(pokemon.name)
+    }
+
     return {
         add: add,
-        getAll: getAll
+        getAll: getAll,
+        addListItem: addListItem,
+        showDetails: showDetails
     };
 })();
 
 //forEach method to iterate through and write pokemon
-function showPokemon(pokemon) {
-    if (pokemon.height >1.4){
-        document.write(pokemon.name + ' height:' + pokemon.height + ' - Wow, thats big! </br>');
-    }else {
-        document.write(pokemon.name + ' height:' + pokemon.height + '</br>');
-    } 
-}
-let getPokemon = pokemonRepository.getAll()
-getPokemon.forEach(showPokemon);
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
+});
+
+
+
